576d51ecfbcd8eae1fb50125eb0f3263f308a68a
Yaroslav <yaroslavsim@ukr.net>
2021-01-27 10:45:21 -0800

    corrected the code under the comments!

--- main.py
+++ main.py
@@ -13,33 +13,16 @@ def check_exchange_rates(currency_type):
     return float(rates)
 
 
-def check_str_to_float(number):
-    try:
-        float(number)
-        return True
-    except:
-        return False
-
-
 def write_result_to_file(name_file, data):
-    current_path = os.getcwd()
-    path_file_history = os.path.join(current_path, name_file)
-    if os.path.isfile(name_file) is True:
-        with open(path_file_history, 'a') as file:
-            file.write("{}\n".format(data))
-    else:
-        with open(path_file_history, 'w') as file:
-            file.write("{}\n".format(data))
+    with open(name_file, 'a') as file:
+        file.write("{}\n".format(data))
 
 
-def check_exchange_result(status_check_str_to_float, amount, exchange_rates, currency_to):
-    if status_check_str_to_float is True:
-        result = round(float(amount) * exchange_rates, 2)
-        data_to_file = "{},{},{},{}".format(currency_to, exchange_rates, amount, result)
-        write_result_to_file('history.log', data_to_file)
-        return f'You received: <b>{result}</b>'
-    if status_check_str_to_float is False:
-        return f'You entered an incorrect amount: <b>{amount}</b>'
+def check_exchange_result(amount, exchange_rates, currency_to):
+    result = round(amount * exchange_rates, 2)
+    data_to_file = "{},{},{},{}".format(currency_to, exchange_rates, amount, result)
+    write_result_to_file('history.log', data_to_file)
+    return f'You received: <b>{result}</b>'
 
 
 @app.route('/')
@@ -47,39 +30,37 @@ def index():
     return 'A web application that converts a certain amount of EUR into various currencies!'
 
 
-@app.route('/eur_to_usd/<amount>')
+@app.route('/eur_to_usd/<float:amount>')
+@app.route('/eur_to_usd/<int:amount>')
 def get_eur_to_usd(amount):
     currency_to = 'USD'
     exchange_rates = check_exchange_rates(currency_to)
-    check_enter_sum = check_str_to_float(amount)
-    return check_exchange_result(check_enter_sum, amount, exchange_rates, currency_to)
+    return check_exchange_result(amount, exchange_rates, currency_to)
 
 
-@app.route('/eur_to_gbp/<amount>')
+@app.route('/eur_to_gbp/<float:amount>')
+@app.route('/eur_to_gbp/<int:amount>')
 def get_eur_to_gbp(amount):
     currency_to = 'GBP'
     exchange_rates = check_exchange_rates('GBP')
-    check_enter_sum = check_str_to_float(amount)
-    return check_exchange_result(check_enter_sum, amount, exchange_rates, currency_to)
+    return check_exchange_result(amount, exchange_rates, currency_to)
 
 
-@app.route('/eur_to_php/<amount>')
+@app.route('/eur_to_php/<float:amount>')
+@app.route('/eur_to_php/<int:amount>')
 def get_eur_to_php(amount):
     currency_to = 'PHP'
     exchange_rates = check_exchange_rates('PHP')
-    check_enter_sum = check_str_to_float(amount)
-    return check_exchange_result(check_enter_sum, amount, exchange_rates, currency_to)
+    return check_exchange_result(amount, exchange_rates, currency_to)
 
 
 @app.route('/history/')
 def show_file_history():
     name_file = 'history.log'
-    current_path = os.getcwd()
-    path_file_history = os.path.join(current_path, name_file)
     if os.path.isfile(name_file) is False:
         return f'History file is missing!'
     if os.path.isfile(name_file) is True:
-        with open(path_file_history,"r") as file:
+        with open(name_file,"r") as file:
             content = file.readlines()
             return render_template('history.html', content=content)
 
