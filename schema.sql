6e727306f86de3227fbccd8339cf48fa55cbefda
YaroslavSim <77851820+YaroslavSim@users.noreply.github.com>
2021-02-06 19:01:28 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS exchange;
-
-CREATE TABLE exchange (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    currency_to TEXT NOT NULL,
-    exchange_rate REAL NOT NULL,
-    amount REAL NOT NULL,
-    result REAL NOT NULL
-);
